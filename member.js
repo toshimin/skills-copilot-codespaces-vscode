aa588ad6667556696a273c222b3cd73bb4614894
toshimin <32206806+toshimin@users.noreply.github.com>
2023-10-21 10:00:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#fff";
+    skillsBtn.style.backgroundColor = "#f5f5f5";
+    }
